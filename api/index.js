1fbd4221328ad3d7c8628b474b70e828807a4d62
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-03-03 21:33:32 -0800

    req res

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const ogs = require('open-graph-scraper')
 
-const options = { url: req.query['url'], timeout: 4000 }
-ogs(options).then((data) => {
-  const { error, result, response } = data
-  res.json(result)
-  console.log('error:', error) // This returns true or false. True if there was an error. The error itself is inside the results object.
-  console.log('result:', result) // This contains all of the Open Graph results
-  console.log('response:', response) // This contains the HTML of page
-})
+module.exports = async (req, res) => {
+  const options = { url: req.query['url'], timeout: 4000 }
+  ogs(options).then((data) => {
+    const { error, result, response } = data
+    res.json(result)
+    console.log('error:', error) // This returns true or false. True if there was an error. The error itself is inside the results object.
+    console.log('result:', result) // This contains all of the Open Graph results
+    console.log('response:', response) // This contains the HTML of page
+  })
+}
