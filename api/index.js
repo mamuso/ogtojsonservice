54abc015a6d9fc63396df726b8d777d0a62a1449
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-03-03 21:24:34 -0800

    Removing consoles

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const ogs = require("open-graph-scraper");
+const ogs = require('open-graph-scraper')
 
 module.exports = async (req, res) => {
-  const options = { url: req.query["url"], timeout: 4000 };
-  let response = await ogs(options);
-  console.log(response);
-  console.log("ok");
-  res.json(response);
-};
+  const options = { url: req.query['url'], timeout: 4000 }
+  let response = await ogs(options)
+  // console.log(response);
+  // console.log("ok");
+  res.json(response)
+}
