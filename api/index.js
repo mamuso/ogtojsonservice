a90bd143b2ffeb11f48530fd918becad521a5f5e
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-03-03 21:26:58 -0800

    promise rejected

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const ogs = require('open-graph-scraper')
 module.exports = async (req, res) => {
   const options = { url: req.query['url'], timeout: 4000 }
   let response = await ogs(options)
-  // console.log(response);
-  // console.log("ok");
-  res.json(response)
+    .then(function (response) {
+      console.log('Promise Resolved')
+      res.json(response)
+    })
+    .catch(function () {
+      console.log('Promise Rejected')
+    })
 }
